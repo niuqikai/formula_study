b2d007f5ee574180746a8fb19b64e185a7ba334d
niuqikai <niuqikai@gmail.com>
2021-11-30 20:39:39 +0800

    ‘20211130’

--- Data_input.py
+++ Data_input.py
@@ -1,4 +1,4 @@
-#此程序为数据输入接口，处理各种格式的数据并且转换为矩阵
+#此程序为数据输入接口，处理各种格式的数据并且转换为矩阵或者图格式
 #
 import pandas as pd
 import networkx as nx
@@ -10,6 +10,55 @@ def datafromcsv(fileapath):
 def data_from_excel_sheet(filepath, st_name):
     df = pd.read_excel(filepath, sheet_name = st_name)  # 可以通过sheet_name来指定读取的表单
     return df
+
+def disease_target(filepath , filename):#根据疾病名称读取疾病，靶点矩阵,疾病名称放在diseasename.csv里面
+    disease_t = 'v_Targets_Diseases'
+    disease_targ = data_from_excel_sheet(filepath + filename, disease_t)  # 计算中药对应的成分
+    #'disease_name'
+    disease_file = 'diseasename.csv'
+    disease_list = pd.read_csv(disease_file, sep = ';')
+    disease_tar =disease_targ[disease_targ['disease_name'].isin(list(disease_list['disease_name']))]
+    return disease_tar
+
+def target_mol(filepath , filename, tar = 'all'): #根据指定的靶点找出相对应的成分，all为默认的全量数据
+    target_m = 'v_Molecules_Targets'
+    target_molecule = data_from_excel_sheet(filepath + filename, target_m)
+
+    if tar == 'all':
+        return target_molecule
+    else:
+        dt = disease_target(filepath ,filename)
+        target_molecule = target_molecule[target_molecule['TARGET_ID'].isin(list(dt['target_ID']))]
+        return  target_molecule
+
+def targets_mol_herb(filepath, filename):#生成目标靶点对应的成分和中药的矩阵
+    target_molecules = target_mol(filepath ,filename, 0)  # 靶点成分对应关系
+    mol_herb = herb_molecules(filepath , filename)  # 成分和中药的对应关系
+
+    targ_mol_herb = pd.merge(target_molecules, mol_herb, how = 'left',on= 'MOL_ID') #将疾病有关的靶点 成分 中药进行关联
+    return targ_mol_herb
+
+def herb_molecules(filepath , filename):#计算中药和成分对应的矩阵
+    herb_m = 'v_Herbs_Molecules'
+    herb_mol = data_from_excel_sheet(filepath + filename, herb_m)  # 计算中药对应的成分
+    return herb_mol
+
+def Graph_from_data():# 将同一疾病的靶点连线，构成图
+    filepath = 'D:\\ctm_data\\TCMSP-数据\\'
+    filename = 'TCMSP_DB_加工.xlsx'
+    sheet_name = 'v_Targets_Diseases'
+    tag_id = 'target_ID'
+    dis_id = 'disease_ID'
+    G = data_from_excel_graph(filepath + filename, sheet_name, tag_id, dis_id)  # 将同一疾病的靶点连线，构成图
+    return G
+
+def herb_mol_targets(filepath,filename):#计算每种中药对应的成分和靶点
+    herb_mol = herb_molecules(filepath , filename)  # 计算中药对应的成分
+    mol_target = target_mol(filepath , filename)  # 成分对应的靶点
+
+    herb_mol_target = pd.merge(herb_mol, mol_target,how = 'left',on= 'MOL_ID') #将中药 成分和成分对应的靶点进行关联
+    #return herb_mol_target
+    return herb_mol_target
 
 def data_from_excel_graph(filepath, st_name, tag_id ,disease_id):#根据Excel生成图
     #disease_ID
--- formula_herb_importance.py
+++ formula_herb_importance.py
@@ -1,56 +1,24 @@
-#计算方剂中节点重要性和配伍重要性
+#算法模块，计算算法过程中的各种指标和变量
 import formula_study.Data_input as di
 import pandas as pd
 import networkx as nx
 import numpy as np
 
 def jaccard_gini(targ_mol_herb):#计算jaccard距离,A为对应靶点成分的数量，药物成分的数量
-    herb_m = 'v_Herbs_Molecules'
-    herb_mol = di.data_from_excel_sheet(filepath + filename, herb_m)  # 计算中药对应的成分
+    herb_mol = di.herb_molecules(filepath , filename)  # 中药对应的成分
     herb_num = herb_mol.groupby('herb_cn_name')['MOL_ID'].nunique()
-
     targ_mol,targ_herb = targ_mol_herb_num(targ_mol_herb)
-
     #疾病对应3001个成分
-    disease_mol_num = 3001
+
+    disease_mol_num = targ_mol_herb['MOL_ID'].nunique()
     jac_gini_matrix = pd.merge(targ_mol.reset_index(), herb_num.reset_index() ,how= 'left', on= 'herb_cn_name')
-    jac_gini_matrix['jaccard_score'] = jac_gini_matrix['MOL_ID'] / (jac_gini_matrix['03_Info_Molecules_MOL_ID'] + disease_mol_num - jac_gini_matrix['MOL_ID'] )
-    jac_gini_matrix['gini_score'] = jac_gini_matrix['MOL_ID'] / jac_gini_matrix['03_Info_Molecules_MOL_ID'].apply(lambda x : disease_mol_num if disease_mol_num < x else x)
+    jac_gini_matrix['jaccard_score'] = jac_gini_matrix['MOL_ID_x'] / (jac_gini_matrix['MOL_ID_y'] + disease_mol_num - jac_gini_matrix['MOL_ID_x'] )
+    jac_gini_matrix['gini_score'] = jac_gini_matrix['MOL_ID_x'] / jac_gini_matrix['MOL_ID_y'].apply(lambda x : disease_mol_num if disease_mol_num < x else x)
     rs = jac_gini_matrix.loc[:,['herb_cn_name','jaccard_score','gini_score']]
-    #rs.to_csv('jaccard_gimi.csv')
-
-def Graph_from_data():# 将同一疾病的靶点连线，构成图
-    filepath = 'D:\\ctm_data\\TCMSP-数据\\'
-    filename = 'TCMSP_DB_加工.xlsx'
-    sheet_name = 'v_Targets_Diseases'
-    tag_id = 'target_ID'
-    dis_id = 'disease_ID'
-    G = di.data_from_excel_graph(filepath + filename, sheet_name, tag_id, dis_id)  # 将同一疾病的靶点连线，构成图
-    return G
-
-def herb_mol_targets(filepath,filename):#计算每种中药对应的成分和靶点
-    herb_m = 'v_Herbs_Molecules'
-    herb_mol = di.data_from_excel_sheet(filepath + filename, herb_m)  # 计算中药对应的成分
-    mols_t = 'v_Molecules_Targets'
-    mol_target = di.data_from_excel_sheet(filepath + filename, mols_t)  # 计算中药对应的成分
-
-    herb_mol_target = pd.merge(herb_mol, mol_target,how = 'left',on= 'MOL_ID') #将中药 成分和成分对应的靶点进行关联
-    #return herb_mol_target
-    return herb_mol_target
-
-def targets_mol_herb(filepath, filename):#生成目标靶点对应的成分和中药的矩阵
-    target_m = 'target_mol'
-    target_mol = di.data_from_excel_sheet(filepath + filename, target_m)  # 靶点成分对应关系
-
-    mol_d = 'mol_disease'
-    mol_disease = di.data_from_excel_sheet(filepath + filename, mol_d)  # 成分和中药的对应关系
-
-    targ_mol_herb = pd.merge(target_mol,mol_disease,how = 'left',on= 'MOL_ID') #将疾病有关的靶点 成分 中药进行关联
-    return targ_mol_herb
-
+    #rs.to_csv('jaccard_gini.csv')
 
 def pagerank_score(tag_mol_herb):#计算PageRank加权之后的每种中药得分
-    G = Graph_from_data()
+    G = di.Graph_from_data()
     pagerank_rs = nx.pagerank(G)
     herb_pagerank = {}
     for herb in tag_mol_herb['herb_cn_name'].unique():
@@ -69,10 +37,10 @@ def targ_mol_herb_num(targ_mol_herb):#计算每种药物对应的靶点成分数
     targ_herb = targ_mol_herb.groupby('herb_cn_name')['TARGET_ID'].nunique()#计算每种药物中 能够关联的靶点数量
     return targ_mol,targ_herb
 
-def shortest_distance(herb_mol_target):
+def shortest_distance(herb_mol_target):#计算两味中药之前的平均最短路径
     filewrite = 'distance.csv'
     herbs = list(herb_mol_target['herb_cn_name'].unique())
-    G = Graph_from_data()
+    G = di.Graph_from_data()
     for i in range(len(herbs)-1):
         for j in range(i+1 , len(herbs) ):
             #print(herbs[i],herbs[j])
@@ -97,23 +65,33 @@ def shortest_distance(herb_mol_target):
                     fw.write('\n')
                     fw.flush()
 
+def walk_score_algorithm(df_data ,source, target):#计算随机游走的分数，df_data为二分网矩阵，source target为源和目标,walk_score为随机游走得分，初始化为1
+    t_m_group = dict(df_data.groupby(source)[target].nunique())
+    df_data['walk_score'] = df_data.apply(lambda x: (1.0/t_m_group[x[source]] * x['walk_score'] if x[source] in t_m_group else 0) ,axis=1)
+    df_data = df_data.groupby(target)['walk_score'].sum()
+    return df_data.reset_index()
 
 if __name__ == '__main__':
     filepath = 'D:\\ctm_data\\TCMSP-数据\\'
     filename = 'TCMSP_DB_加工.xlsx'
 
-    targ_mol_herb = targets_mol_herb(filepath,filename)#疾病靶点对应的
-    herb_mol_target = herb_mol_targets(filepath, filename) # 计算每种中药对应的成分和靶点
+    targ_mol_herb = di.targets_mol_herb(filepath,filename)#疾病靶点对应的
+    herb_mol_target = di.herb_mol_targets(filepath, filename) # 计算每种中药对应的成分和靶点
+    target_molecule = di.target_mol(filepath, filename, tar='0') #和疾病关联的靶点以及成分
+    herb_mols =  di.herb_molecules(filepath, filename) #中药对应的成分
 
 
-    #for herb_name in herb_mol_target['herb_cn_name'].unique():
-    #    print(herb_name)
-    #    print((herb_mol_target[herb_mol_target['herb_cn_name'] == herb_name]['TARGET_ID']))
-
-    #filewrite = 'targ_herb.csv'
-    #pd.DataFrame(targ_herb).to_csv(filewrite)
-    #jaccard_gini(targ_mol,herb_num)
-    #pagerank_score(targ_mol_herb)
+    t_m = target_molecule[['TARGET_ID','MOL_ID']].drop_duplicates()# 提取靶点和成分列，第一列为起点列
+    source = 'TARGET_ID'
+    target = 'MOL_ID'
+    t_m['walk_score'] = t_m[source].apply(lambda x: 1.0)#初始化分数
+    mols_score = walk_score_algorithm(t_m, source ,target)  #成分和对应的分数
+    mols_score_dict = {key:values for key, values in zip(mols_score['MOL_ID'], mols_score['walk_score'])}#转换为字典结构
 
 
-
+    herb_mols_values = herb_mols[['MOL_ID','herb_cn_name']].drop_duplicates()
+    herb_mols_values['walk_score'] = herb_mols_values['MOL_ID'].apply(lambda x: mols_score_dict[x] if x in mols_score_dict else 0)
+    source = 'MOL_ID'
+    target = 'herb_cn_name'
+    herb_score = walk_score_algorithm(herb_mols_values, source ,target)  #药物和对应的分数
+    herb_score.to_csv('herb_score.csv')
